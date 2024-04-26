c436a12fa9ed7fcf85d3e44b9ae2510452ef68d0
steekam <waynewanyee@gmail.com>
2024-04-26 09:02:36 +0300

    wip: current weather section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React, {useContext} from 'react';
+import React, {useMemo} from 'react';
 import {getWeatherInfo} from "./lib/api";
 import {useQuery} from "@tanstack/react-query";
-import {IntlProvider} from "react-intl";
-import {AppContext} from "./lib/hooks/app-context";
+import {formatDate, formatISO, fromUnixTime} from "date-fns";
 
 
 function App() {
-    // const {loading: waitingForLocation, latitude, longitude} = useGeolocation();
     const waitingForLocation = false;
     const latitude = -1.286389;
     const longitude = 36.817223;
@@ -15,31 +13,96 @@ function App() {
         queryKey: ["weather", {latitude, longitude}] as const,
         queryFn: async ({queryKey}) => {
             const {latitude, longitude} = queryKey[1];
-            const res = await getWeatherInfo({lon: longitude!, lat: latitude!, exclude: ["daily", "alerts", "hourly"]});
+            const res = await getWeatherInfo({lon: longitude!, lat: latitude!, exclude: ["alerts", "hourly"]});
             return res.data;
         },
         enabled: Boolean(!waitingForLocation && (latitude && longitude)),
+        staleTime: 10 * 60 * 1000,
     });
 
-    const {locale, updateLocale} = useContext(AppContext);
+    if (data) {
+        console.log(data);
+    }
+
+    const currentDerived = useMemo(() => {
+        if (!data) {
+            return null;
+        }
+
+        return {
+            min: data.daily[0].temp.min,
+            max: data.daily[0].temp.max,
+            chanceOfRain: data.daily[0].pop * 100,
+        };
+    }, [data]);
+
+    const today = new Date;
 
     return (
-        <IntlProvider defaultLocale={"en"} locale={locale}>
-            <label htmlFor="language-switcher">Language</label>
-            <select name="lang" id="language-switcher" onChange={(e) => updateLocale(e.target.value)}>
-                <option value="en">English</option>
-                <option value="sw">Swahili</option>
-            </select>
-            Current Locale: {locale}
-            {
-                (isLoading || waitingForLocation) ? "Loading..." : (<>
-                    <h2>Your weather data: </h2>
-                    <pre style={{padding: '10px', backgroundColor: "var(--gray-contrast)", color: "var(--gray-1)"}}>
-                        {JSON.stringify(data.current, null, 2)}
-                    </pre>
-                </>)
-            }
-        </IntlProvider>
+        <main className={"wrapper"}>
+            <div className={"container"}>
+                <section className={"current-section"}>
+                    <div>
+                        <small>
+                            <time dateTime={formatISO(today)} className={"current-weather-date"}>
+                                {formatDate(today, "EEEE, do MMMM yyy")}
+                            </time>
+                        </small>
+                        <h2 className={"location-title"}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 strokeWidth={1.5}
+                                 stroke="currentColor" className="location-icon">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"/>
+                            </svg>
+
+                            <span>Nairobi, Kenya</span>
+                        </h2>
+                    </div>
+
+                    {
+                        isLoading ? (
+                            <div className={"skeleton"}>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>) : (
+                            <>
+                                <div className={"weather-info"}>
+                                    <img className={"icon"}
+                                         src={`https://openweathermap.org/img/wn/${data.current.weather[0].icon}@4x.png`}
+                                         alt={`${data.current.weather[0].description}`}/>
+
+                                    <p className={"temperature"}>{~~data.current.temp}<sup>&deg;C</sup></p>
+
+                                    <p className={"description"}>
+                                        {data.current.weather[0].main}
+                                        <span className={"temps"}>
+                                            {~~currentDerived?.min}<sup>&deg;C</sup> / {~~currentDerived?.max}<sup>&deg;C</sup>
+                                        </span>
+                                    </p>
+
+                                </div>
+                                <p className={"last-updated-at"}>
+                                    Last updated:
+                                    <time dateTime={formatISO(fromUnixTime(data.current.dt))}>
+                                        {formatDate(fromUnixTime(data.current.dt), "do MMM, yyyy HH:mm")}
+                                    </time>
+                                </p>
+                            </>
+                        )
+
+                    }
+
+                </section>
+
+                <section className={"detailed-section"}>
+                    Extra information
+                </section>
+            </div>
+        </main>
     );
 }
 
--- src/lib/api/schemas.ts
+++ src/lib/api/schemas.ts
@@ -1,1 +1,7 @@
+export interface WeatherApiError {
+    cod: number;
+    message: string;
+    parameters?: Array<string>;
+}
+
 export {}
